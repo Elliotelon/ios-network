7ada9117a8ac37239e0ea7e05aab4d6c8092c744
minkyu <elliotelon@gmail.com>
2026-01-11 17:05:36 +0900

    Initial Commit

---
+++ IosNetwork/IosNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosNetworkApp.swift
+//  IosNetwork
+//
+//  Created by 김민규 on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct IosNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
